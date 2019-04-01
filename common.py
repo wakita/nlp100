4e08ec1e70ef6270ff92e9a8863ac0ca8cdeb5ec
Ken Wakita <wakita@is.titech.ac.jp>
2019-04-01 22:07:12 +0900

    Revert "Update common.py"
    This reverts commit 3f95a843b33f506686c9cb3e00f701920a286d3e.

--- common.py
+++ common.py
@@ -3,14 +3,3 @@ def chapter(t):
 
 def title(t):
     print(f'\n# Problem - {t}\n')
-
-import subprocess
-
-def system(command, **options):
-    proc = subprocess.Popen(command, shell=True,
-                            stdout=subprocess.PIPE,
-                            **options)
-    return proc.communicate()[0].decode('utf-8')
-
-def cat(filepath):
-    return system(f'cat {filepath}')
