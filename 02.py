7bc898499b75c3b5af2dddf1cef55e9dc32533c4
Ken Wakita <wakita@is.titech.ac.jp>
2019-04-07 22:16:14 +0900

    deleted an unused variable

--- 02.py
+++ 02.py
@@ -59,8 +59,6 @@ title('12. 1列目をcol1.txtに，2列目をcol2.txtに保存')
 
 # 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
 
-lines = [line.split('\t') for line in text_lines]
-
 def split(r, col1, col2):
     for line in r:
         line1, line2 = line.split('\t')[:2]
